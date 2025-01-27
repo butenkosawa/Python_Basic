1fc3bc81071638c50cb542b2d9d046cc24b09135
butenkosawa <butenkosawa@gmail.com>
2025-01-27 17:23:36 +0200

    Add Dir Python_Book

--- 02/01.py
+++ 02/01.py
@@ -1,1 +1,19 @@
 # lesson 2
+a = 1986
+print(a)
+print("id(a):", id(a))
+
+b = a
+print(b)
+print("id(b):", id(b))
+print()
+
+a = "Вітаю!"
+print(a)
+print("id(a):", id(a))
+print("id(b):", id(b))
+print()
+
+b = 100
+print(b)
+print("id(b):", id(b))
