528ed96193fdfd1ba652217990b87c529b50232e
butenkosawa <butenkosawa@gmail.com>
2025-02-05 11:40:38 +0200

    Add DZ 4.2, 4.3

--- Domashky/4.2.py
+++ Domashky/4.2.py
@@ -1,7 +1,7 @@
 lst = [1, 3, 5, 10]
 
 if len(lst) > 0:
-    result = sum(lst[::2]) * lst.pop()
+    result = sum(lst[::2]) * lst[-1]
 else:
     result = len(lst)
 
---
+++ Domashky/4.3.py
@@ -0,0 +1,4 @@
+lst = [1, 2, 3, 4, 5, 6, 7, 9]
+lst_1 = [lst[0], lst[2], lst[-2]]
+print(lst)
+print(lst_1)
