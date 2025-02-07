707a938bae19f9b6a73782debbdc6a7a43069e0b
butenkosawa <butenkosawa@gmail.com>
2025-02-07 06:23:12 +0200

    Add DZ 4.3

--- Domashky/4.3.py
+++ Domashky/4.3.py
@@ -3,7 +3,7 @@ import random
 len_lst = random.randint(3,10)
 lst = []
 
-for lst_elem in range(0, len_lst):
+for lst_elem in range(len_lst):
     lst.append(random.randint(0,99))
 
 lst_1 = [lst[0], lst[2], lst[-2]]
