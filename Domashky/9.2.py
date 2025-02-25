d73d2df3eb7c0fd28a9cdf46ce3805728df3bbb7
butenkosawa <butenkosawa@gmail.com>
2025-02-25 10:54:31 +0200

    Add DZ 9.2

--- Domashky/9.2.py
+++ Domashky/9.2.py
@@ -18,6 +18,7 @@
 def difference(*args):
     return 0 if args == () else round(max(args) - min(args), 2)
 
+
 print(difference(1, 2, 3))
 print(difference(5, -5))
 print(difference(10.2, -2.2, 0, 1.1, 0.5))
